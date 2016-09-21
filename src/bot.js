1bf77f23a54da9f435ab1ec3160b23b7bb9f24ab
Ryan Faulhaber <faulhaber.14@osu.edu>
2016-09-21 14:49:04 -0400

    made it so that it only tells one fact per day

--- src/bot.js
+++ src/bot.js
@@ -7,6 +7,8 @@ var Breadbot = function(token) {
     this.token = token;
     this.controller = Botkit.slackbot();
     this.bot = this.spawn();
+    this.lastBreadFact;
+    this.lastJoke;
 }
 
 Breadbot.prototype.spawn = function() {
@@ -16,6 +18,8 @@ Breadbot.prototype.spawn = function() {
 }
 
 Breadbot.prototype.initialize = function() {
+    let hearsKeywords = ['bread fact', ':bread:', 'joke'];
+
     this.bot.startRTM(function(error, bot, payload) {
         if (error) {
             console.log("Couldn't connect to Slack!");
@@ -24,20 +28,58 @@ Breadbot.prototype.initialize = function() {
         }
     });
 
-    let keywords = ['bread fact', ':bread:', 'joke'];
-    let events = ['direct_mention'];
 
-    this.controller.hears(keywords, events, function(bot, message) {
+    this.controller.hears(hearsKeywords, ['direct_mention'], function(bot, message) {
+        handleDirect(bot, message);
+    });
+
+    this.controller.on('direct_message', function(bot, message) {
+        handleDirect(bot, message);
+    });
+
+    function handleDirect(bot, message) {
         switch(message.match[0]) {
-            case keywords[0]:
-            case keywords[1]:
-                console.log("I must tell a bread fact!");
+            case hearsKeywords[0]:
+            case hearsKeywords[1]:
+                handleBreadPrompt(bot, message);
                 break;
-            case keywords[2]:
-                console.log("I must tell a joke!");
+            case hearsKeywords[2]:
+                handleJokePrompt(bot, message);
                 break;
         }
-    });
+    }
+
+    function handleBreadPrompt(bot, message) {
+        console.log("I must tell a bread fact!");
+        let breadFactDate = Breadbot.lastBreadFact;
+
+        if (breadFactDate == undefined || hoursSince(breadFactDate.getTime()) >= 24) {
+            bot.reply(message, getBreadFact());
+            Breadbot.lastBreadFact = new Date();
+        } else {
+            bot.reply(message, getNoBreadMessage());
+        }
+    }
+
+    function handleJokePrompt(bot, message) {
+        console.log("I must tell a joke!");
+        bot.reply(message, getJoke());
+    }
+
+    function hoursSince(time) {
+        var t = Math.abs(time - new Date().getTime()) / (1000 * 3600);
+        return t;
+    }
+
+    function getBreadFact() {
+        return "Bread is a food!"; // TODO: change placeholder text
+    }
+
+    function getNoBreadMessage() {
+        var timeUntil = (24 - hoursSince(Breadbot.lastBreadFact.getTime())).toFixed(2);
+        return "Sorry, I only tell one bread fact a day! You must wait " +
+             timeUntil + " hours to hear another one!";
+    }
 }
 
 module.exports = {
